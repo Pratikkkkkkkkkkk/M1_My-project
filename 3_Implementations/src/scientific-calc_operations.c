596a4dd0e4fb69a2c9b16147d95c01ad800c142b
Pratikkkkkkkkkkk <101550668+Pratikkkkkkkkkkk@users.noreply.github.com>
2022-04-01 21:09:18 +0530

    Update scientific-calc_operations.c

--- 3_Implementations/src/scientific-calc_operations.c
+++ 3_Implementations/src/scientific-calc_operations.c
@@ -1,1 +1,37 @@
+#include <scientific-calc_operations.h>
 
+int add(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int subtract(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int multiply(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int divide(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+} 
+  
+  int modulous(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+  int add(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+  
+    else
+        return operand1 / operand2;
+}
